7c753249fb334a7f349f8753f958a01477e842c8
KergmaHyper <kergma@gmail.com>
2021-05-13 00:33:36 +0300

    stream collection

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,22 @@
+import java.util.ArrayList;
+
+import java.util.Collections;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        ArrayList<String> otd = new ArrayList<>();
+
+        otd.add("feo");
+        otd.add("ek");
+        otd.add("it");
+        otd.add("inkass");
+        otd.add("buh");
+        otd.add("zahist");
+        Collections.addAll(otd,"bud","mmsb","okr","corp","bezpeka","HR","back","valut","finmon");
+
+        otd.stream().filter(s->s.length()<3).sorted().forEach(s->System.out.println(s));
+
+
     }
 }
